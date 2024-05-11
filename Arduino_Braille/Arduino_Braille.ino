2a17221153f494125183b5a2db8e6f376a9304ea
7angJung <peterj0117@naver.com>
2024-05-11 20:51:54 +0900

    11th Commit

--- Arduino_Braille/Arduino_Braille.ino
+++ Arduino_Braille/Arduino_Braille.ino
@@ -1,3 +1,4 @@
+#include "braille.h"
 
 int buttonPin1 = 2;  // 버튼 1 연결 핀
 int buttonPin2 = 3;  // 버튼 2 연결 핀
@@ -5,21 +6,22 @@ int buttonPin3 = 4;  // 버튼 3 연결 핀
 
 void setup() {
   Serial.begin(9600); // 시리얼 통신 속도 설정
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(buttonPin3, INPUT_PULLUP);
 }
 
+
 void loop() {
-  if (digitalRead(buttonPin1) == HIGH) {
+  if (digitalRead(buttonPin1) == LOW) { // 버튼이 눌렸을 때
     Serial.println("Take Picture");
     delay(1000); // 디바운싱을 위한 딜레이
   }
-  else if (digitalRead(buttonPin2) == HIGH) {
+  else if (digitalRead(buttonPin2) == LOW) { // 버튼이 눌렸을 때
     Serial.println("Next");
     delay(1000); // 디바운싱을 위한 딜레이
   }
-  else if (digitalRead(buttonPin3) == HIGH) {
+  else if (digitalRead(buttonPin3) == LOW) { // 버튼이 눌렸을 때
     Serial.println("Previous");
     delay(1000); // 디바운싱을 위한 딜레이
   }
