7c6cd3d2bb43510c7a208679965ab9e264c888bd
7angJung <peterj0117@naver.com>
2024-05-29 22:07:42 +0900

    Initial Commit

--- camera_operation.py
+++ camera_operation.py
@@ -14,18 +14,17 @@ get_ipython().system('pip install opencv-python')
 import cv2
 import time
 
-def taking_picture():
+def taking_picture(camera):
     # 기본 img_path 값을 설정. 이미지 저장에 실패할 경우 반환할 값입니다.
     img_path = None
-    camera = cv2.VideoCapture(2)
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    print("\nTaking picture...")
+    print("\n이미지 촬영 진행 중입니다.")
     ret, frame = camera.read()
     if ret:
         timestamp = time.strftime("%Y%m%d-%H%M%S")
         img_path = f"C:\\Users\\peter\\capstone\\camera\\{timestamp}.jpg"
         cv2.imwrite(img_path, frame)
-        print(f"Picture Path : {img_path}\n")
+        print(f"\n이미지 경로 : {img_path}\n")
     return img_path
 
--- image_processing.py
+++ image_processing.py
@@ -21,57 +21,9 @@ get_ipython().system('git clone https://github.com/ultralytics/yolov5')
 # In[1]:
 
 
-"""
 import torch
 import cv2
-import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw, ImageFont, ImageEnhance
-import sys
-
-def crop_book(image_path, left_half_path, right_half_path):
-    # 모델 로드
-    model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # YOLOv5 모델 버전 선택
-
-    # 추론 수행
-    results = model(image_path)
-
-    # detected된 모든 객체를 pandas 데이터프레임 형태로 변환
-    df = results.pandas().xyxy[0]
-
-    # 'book' 클래스의 객체들만 필터링
-    book_df = df[df['name'] == 'book']
-
-    # confidence가 가장 높은 'book' 객체 찾기
-    if not book_df.empty:
-        highest_conf_row = book_df.loc[book_df['confidence'].idxmax()]
-        
-        # 이미지 로드
-        img = Image.open(image_path)
-        
-        # 바운딩 박스로 이미지 자르기
-        cropped = img.crop((int(highest_conf_row['xmin']), int(highest_conf_row['ymin']), int(highest_conf_row['xmax']), int(highest_conf_row['ymax'])))
-        
-        # 이미지를 가로로 반으로 나누기
-        width, height = cropped.size
-        left_half = cropped.crop((0, 0, width/2, height))
-        right_half = cropped.crop((width/2, 0, width, height))
-        
-        # 나눠진 이미지 파일 저장
-        left_half.save(left_half_path)
-        right_half.save(right_half_path)
-        
-        return True  # 성공적으로 처리됨
-    else:
-        print("탐지된 'book'이 없습니다.")
-        return False  # 'book'이 탐지되지 않음
-"""
-
-
-# In[3]:
-
-
-import torch
-import cv2
+import os
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 import sys
@@ -94,42 +46,45 @@ def crop_image(image_path):
 
         # 이미지 로드
         img = Image.open(image_path)
+
+        # 이미지 저장 전 파일명 수정
+        img_filename = os.path.splitext(os.path.basename(image_path))[0]
+        base_path = "C:\\Users\\peter\\capstone\\onePage\\"
         
         # 바운딩 박스로 이미지 자르기
         cropped = img.crop((int(highest_conf_row['xmin']), int(highest_conf_row['ymin']), int(highest_conf_row['xmax']), int(highest_conf_row['ymax'])))
         
-        # 자른 이미지 저장
-        cropped_image_path = image_path.replace(".jpg", "_cropped.jpg")
-        cropped.save(cropped_image_path)
-        print(f"이미지를 {cropped_image_path}로 자르고 저장했습니다.")
+        # 책인지 단순 문서인지 구분
+        width, height = cropped.size  # 자른 이미지의 너비와 높이 가져오기
+        ratio = height / width
 
-        # 이미지를 가로로 반으로 나누기
-        width, height = cropped_image_path.size
-        left_half = cropped.crop((0, 0, width/2, height))
-        right_half = cropped.crop((width/2, 0, width, height))
+        if 1.2 < ratio < 1.7:
+            # 문서일 경우, 이미지를 나누지 않고 저장
+            print(f"\n해당 이미지 내에 객체는 문서이거나, 책의 한쪽 페이지입니다.")
+            cropped_image_path = f"{base_path}{img_filename}_cropped.jpg"
+            cropped.save(cropped_image_path)
+            left_half_path = ""
+            right_half_path = ""
+            print(f"\n이미지를 {cropped_image_path}로 자르고 저장했습니다.")
+        else:
+            # 문서가 아닐 경우, 이미지를 가로로 반으로 나누기
+            cropped_image_path = image_path.replace(".jpg", "_cropped.jpg")
+            cropped.save(cropped_image_path)
+            left_half = cropped.crop((0, 0, width/2, height))
+            right_half = cropped.crop((width/2, 0, width, height))
+            
+            # 나눠진 이미지 파일 경로 설정
+            left_half_path = f"{base_path}{img_filename}_left.jpg"
+            right_half_path = f"{base_path}{img_filename}_right.jpg"
+            
+            # 나눠진 이미지 파일 저장
+            left_half.save(left_half_path)
+            right_half.save(right_half_path)
+            print(f"\n이미지를 {cropped_image_path}, {left_half_path}, {right_half_path}로 자르고 저장했습니다.")
         
-        # 나눠진 이미지 파일 저장
-        left_half.save(left_half_path)
-        right_half.save(right_half_path)
-        
-        return True
+        return cropped_image_path, left_half_path, right_half_path
 
     else:
-        print("탐지된 'book'이 없습니다.")
-        return None  # 'book'이 탐지되지 않음
-        
-# 책인지 단순 문서인지 구분을 위한 함수
-def is_document(image_path):
-    # 이미지 열기
-    with Image.open(image_path) as img:
-        width, height = img.size  # 이미지의 너비와 높이 가져오기
-        
-    # 이미지의 비율 계산
-    ratio = height / width
-    
-    # 비율을 기반으로 문서인지 아닌지 판별
-    if 1.2 < ratio < 1.7:
-        return True
-    else:
-        return False
+        print("\n탐지된 'book'이 없습니다.")
+        return None, None, None  # 'book'이 탐지되지 않음
 
